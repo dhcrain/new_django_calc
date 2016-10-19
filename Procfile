7409dce97913d99f0792dbd4de8c38fed362f7c7
dhcrain <dhcrain@gmail.com>
2016-10-19 11:15:36 -0400

    first 4 heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+“web: gunicorn calc_proj.wsgi --log-file -”
--- calc_proj/settings.py
+++ calc_proj/settings.py
@@ -121,3 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
