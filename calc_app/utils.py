4caa93be3aab993caf90972d4f090923e3ac8bc4
Davis Crain <dhcrain@gmail.com>
2016-10-21 15:21:46 -0400

    better error checking on do_math

--- calc_app/utils.py
+++ calc_app/utils.py
@@ -17,8 +17,13 @@ def multiply(a, b):
 
 
 def do_math(num1, operator, num2):
-    if num2 == 0:
+    if num2 == 0 and operator == '/':
         return "Can't Divided By Zero"
     else:
         option = {'+': add, '-': subtract, '/': divide, 'X': multiply}
-        return option[operator](num1, num2)
+        result = option[operator](num1, num2)
+        str_num1 = str(num1)
+        str_num2 = str(num2)
+        if str_num1[:2:-1] == '' and str_num2[:2:-1] == '':
+            return int(result)
+        return round(result, 4)
