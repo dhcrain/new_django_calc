9549714a11890340dfa043ce1b8ff172c294cc70
Davis Crain <dhcrain@gmail.com>
2016-10-24 09:54:26 -0400

    refined how the calculator displays numerbs, ints stay as ints

--- calc_app/models.py
+++ calc_app/models.py
@@ -16,5 +16,17 @@ class Calculation(models.Model):
     def get_result(self):
         return do_math(self.num1, self.operator, self.num2)
 
+    @property
+    def get_num1(self):
+        if self.num1 == int(self.num1):
+            self.num1 = int(self.num1)
+        return self.num1
+
+    @property
+    def get_num2(self):
+        if self.num2 == int(self.num2):
+            self.num2 = int(self.num2)
+        return self.num2
+
     def __str__(self):
         return "{} {} {} = {}".format(self.num1, self.operator, self.num2, self.get_result)
--- calc_app/utils.py
+++ calc_app/utils.py
@@ -22,8 +22,6 @@ def do_math(num1, operator, num2):
     else:
         option = {'+': add, '-': subtract, '/': divide, 'X': multiply}
         result = option[operator](num1, num2)
-        str_num1 = str(num1)
-        str_num2 = str(num2)
-        if str_num1[:2:-1] == '' and str_num2[:2:-1] == '':
+        if result == int(result):
             return int(result)
         return round(result, 4)
